4a02489ec13afeeeea1d05207d940de1171afd22
Esma Beyatli <esma.beytl0683@gmail.com>
2024-12-20 02:05:11 +0300

    Garson ekranına "Tamamla" düğmesi eklendi.

--- KafeYonetimSistemi/Pages/Waiters/Index.cshtml.cs
+++ KafeYonetimSistemi/Pages/Waiters/Index.cshtml.cs
@@ -1,12 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using KafeYonetimSistemi.Models;
 using KafeYonetimSistemi.Data;
 
-
 namespace KafeYonetimSistemi.Pages.Waiters
 {
-
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +25,26 @@ namespace KafeYonetimSistemi.Pages.Waiters
                 .Include(o => o.MenuItems) // Menu ogelerini de dahil et
                 .Include(o => o.Table)     // Masayi da dahil et
                 .ToListAsync();
+        }
+
+        [BindProperty]
+        public int OrderId { get; set; }
+
+        public async Task<IActionResult> OnPostCompleteOrderAsync()
+        {
+            // Ýlgili siparisi bul
+            var order = await _context.Order.FindAsync(OrderId);
+            if (order == null)
+            {
+                return NotFound(); // Siparis bulunamazsa hata dondur
+            }
+
+            // Siparis durumunu guncelle
+            order.Status = OrderStatus.Delivered;
+            await _context.SaveChangesAsync();
+
+            // Kullaniciyi ayni sayfaya yonlendir
+            return RedirectToPage();
         }
     }
 }
