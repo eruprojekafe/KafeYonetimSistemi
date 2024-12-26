131709ea33f43b7fad434eff65d6d33a4722d965
hatice yalçın <yalcn-hatice-357@hotmail.com>
2024-12-26 12:47:04 +0300

    Envanter sayfası yapıldı.

--- KafeYonetimSistemi/Pages/Inventory/Index.cshtml.cs
+++ KafeYonetimSistemi/Pages/Inventory/Index.cshtml.cs
@@ -8,13 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using KafeYonetimSistemi.Data;
 using KafeYonetimSistemi.Models;
 
-
 namespace KafeYonetimSistemi.Pages.Inventory
 {
     public class IndexModel : PageModel
-
     {
-      
         private readonly KafeYonetimSistemi.Data.ApplicationDbContext _context;
 
         public IndexModel(KafeYonetimSistemi.Data.ApplicationDbContext context)
@@ -25,14 +22,11 @@ namespace KafeYonetimSistemi.Pages.Inventory
         public IList<MenuItem> MenuItem { get; set; }
         public IList<MenuItemTransaction> MenuItemTransaction { get; set; }
 
-
         public async Task OnGetAsync()
         {
             MenuItem = await _context.MenuItem
-                                  .Include(m => m.MenuItemTransaction)
-                                  .ToListAsync();
-
+                .Include(m => m.MenuItemTransaction)
+                .ToListAsync();
         }
-
     }
 }
